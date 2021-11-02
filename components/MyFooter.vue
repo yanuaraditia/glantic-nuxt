76b8d60fb79cd9a21efa2b3d58d6726b1ad9458d
Yanuar Aditia <yanuaraditia@outlook.com>
2021-11-02 21:58:45 +0700

    Smaller text

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -2,10 +2,10 @@
   <footer class="py-5">
     <div class="container px-3 px-lg-4">
       <div class="text-lg-center">
-        <p class="mb-1">Contents licensed under <a href="https://griko.dev/cc-by-nc-sa">CC BY-NC-SA 4.0</a>.</p>
-        <p>Made using <a href="//nuxtjs.org">Nuxt.js</a>, <a href="//getbootstrap.com">Bootstrap</a>. Hosted on <a
+        <p class="mb-0 small">Contents licensed under <a href="https://griko.dev/cc-by-nc-sa">CC BY-NC-SA 4.0</a>.</p>
+        <p class="small">Made using <a href="//nuxtjs.org">Nuxt.js</a>, <a href="//getbootstrap.com">Bootstrap</a>. Hosted on <a
           href="//netlify.com">Netlify</a>.</p>
-        <p>
+        <p class="small">
           MIT License © 2021–present <nuxt-link to="/">Yanuar Aditia</nuxt-link>.
         </p>
       </div>
