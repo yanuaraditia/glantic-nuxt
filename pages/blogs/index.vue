70ba041ef6dccab1a28b9579b8423aa084c75dfa
Yanuar Aditia <yanuaraditia@outlook.com>
2021-11-02 15:59:40 +0700

    MissroutE

--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <li v-for="post of posts" :key="post.slug">
-      <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+      <NuxtLink :to="`/blogs/`+post.slug">{{ post.title }}</NuxtLink>
     </li>
   </div>
 </template>
