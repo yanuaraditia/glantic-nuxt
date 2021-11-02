ad250c32ac05906844b0f964e491ccfcf6f56325
Yanuar Aditia <yanuaraditia@outlook.com>
2021-11-02 21:50:53 +0700

    More readable

--- pages/blogs/_slug.vue
+++ pages/blogs/_slug.vue
@@ -1,9 +1,11 @@
 <template>
   <section class="py-5 reader-section">
     <div class="container px-3 px-lg-4">
-      <h1>{{ post.title }}</h1>
-      <div class="mt-5">
-        <nuxt-content :document="post" />
+      <div class="mx-lg-auto" style="max-width: 768px">
+        <h1>{{ post.title }}</h1>
+        <div class="mt-5">
+          <nuxt-content :document="post" />
+        </div>
       </div>
     </div>
   </section>
