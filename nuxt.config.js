859224e4a428fb3c8559758be0b23c28e2b97aa0
Yanuar Aditia <yanuaraditia@outlook.com>
2021-11-02 21:16:08 +0700

    override dikit

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Yanuar Aditia',
+    titleTemplate: '%s — Yanuar Aditia',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
