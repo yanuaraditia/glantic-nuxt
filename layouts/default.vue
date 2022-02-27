35514815a883f77ae3392b9f8126b7f82f4c49bd
Yanuar Aditia <yanuaraditia@outlook.com>
2022-02-27 23:07:28 +0700

    XF

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,10 +9,15 @@
 </template>
 <script>
 export default {
+  created() {
+    if (this.$nuxt.$colorMode.preference === 'system') {
+      this.$nuxt.$colorMode.preference = 'dark'
+    }
+  },
   mounted() {
     if (this.$nuxt.$colorMode.preference === 'system') {
       this.$nuxt.$colorMode.preference = 'dark'
     }
-  }
+  },
 }
 </script>
